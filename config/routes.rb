95d1466da13e249f742c8f6a27b99545c8d9ed0f
Tsengel Purevdorj <tsnglp@gmail.com>
2019-07-25 13:46:14 +1000

    Add sessions create and destroy, update navigation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  root to: 'chatroom#index'
-  get 'login', to: 'sessions#new'
+  root                to: 'chatroom#index'
+  get     'login',    to: 'sessions#new'
+  post    'login',    to: 'sessions#create'
+  delete  'logout',   to: 'sessions#destroy'
 end
